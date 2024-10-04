83616b46485d2fa9f620795b3c0a40ca3b5ac22e
Sayyed Kasif ali <sayyedkashifali586@gmail.com>
2024-10-04 10:55:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import telebot
 import os
 
-# Access the bot token from environment variables
-BOT_TOKEN = os.environ.get('7734029404:AAGjciB3zvBfxMP8XpePT3-mRQLsPAkCY74') 
+# Access the bot token from environment variable 
 
-bot = telebot.TeleBot(BOT_TOKEN)
+BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN) 
+
 
 # Handle the /start command
 @bot.message_handler(commands=['start'])
