4d8e2a2d233e7360a69450dff637fb48a86dda81
Sayyed Kasif ali <sayyedkashifali586@gmail.com>
2024-10-04 13:04:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,29 +9,23 @@ app = Flask(__name__)
 # Pyrogram Bot configuration
 api_id = os.environ.get("27317700")
 api_hash = os.environ.get("de1077f45e29e6abebcd2b9dd196be1d")
-bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
+bot_token = os.environ.get("BOT_TOKEN")
 
 # Pyrogram Client
 bot = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-
 # Telegram Bot Command Handler for /start
 @bot.on_message(filters.command("start"))
 def start_command(client, message):
-    message.reply_text(
-        "Hello! This bot is powered by Flask and Pyrogram. How can I help you today?"
-    )
+    message.reply_text("Hello! This bot is active and running!")  # Simplified response
 
-
-# Flask route for health check or any simple route
+# Flask route for health check
 @app.route("/")
 def index():
     return "Bot is running on Flask!"
 
-
 def run_bot():
     bot.run()
-
 
 if __name__ == "__main__":
     # Running Flask app in main thread
@@ -40,4 +34,3 @@ if __name__ == "__main__":
     # Running Telegram bot in a separate thread
     t2 = threading.Thread(target=run_bot)
     t2.start()
-    
