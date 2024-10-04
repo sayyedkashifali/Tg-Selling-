5d4971c83592525c15a9709223904599750ee189
Sayyed Kasif ali <sayyedkashifali586@gmail.com>
2024-10-04 10:50:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import telebot
 import os
 
-# Get the bot token from environment variables
-BOT_TOKEN = os.environ.get('7734029404:AAGjciB3zvBfxMP8XpePT3-mRQLsPAkCY74')
+# Access the bot token from environment variables
+BOT_TOKEN = os.environ.get('7734029404:AAGjciB3zvBfxMP8XpePT3-mRQLsPAkCY74') 
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # Handle the /start command
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Bot Is Working 💀")
+    bot.reply_to(message, "Welcome to the bot! Products will be added soon.")
 
 # Start listening for messages
 bot.polling()
